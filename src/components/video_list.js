c3998a2d2337220fcc42bda236fd851fa2cf2bca
Jonathan <jonathanprada@protonmail.ch>
2017-03-19 14:34:15 +0100

    Add files via upload

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,3 +1,23 @@
-/**
- * Created by Broski on 18/03/2017.
- */
+import React from 'react';
+import VideoListItem from './video_list_item';
+
+//Data from index.js gets passed in as an object called props
+const VideoList = (props) => {
+    //for reach of the elements in our array, do a function
+    const videoItems = props.videos.map((video) => {
+        //Passes in a video from our array
+        //uses the youtube etag to give each a key
+        return <VideoListItem key={video.etag} video={video}/>
+    })
+
+    //videoItems becomes a list of processed elements
+    //Which are able to reference here
+    //So each element looks like <li>Video</li>
+    return (
+        <ul className="col-md-4 list-group">
+            {videoItems}
+        </ul>
+    );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,50 @@
 //Libraries
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+
+//import functionality from youtube library
+import YTSearch from 'youtube-api-search';
 
 //import our search bar component from the correct file
 import SearchBar from './components/search_bar';
+
+//import our video list component from the correct file
+import VideoList from './components/video_list';
 
 //Youtube data api key
 const API_KEY = 'AIzaSyCLgKK8K7TLZ5TlvHcO_qJQ9AuSrg5uSlo';
 
 //Create our app that will append to the root node
 //inside we render the search bar component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component  {
+    constructor (props) {
+        super(props);
+
+        //videos will contain a list of objects
+        this.state = { videos: []};
+
+        //Our youtube api search
+        YTSearch({
+                key: API_KEY,
+                term: 'Pewdiepie'
+            },
+            //call back function, on success it gets data passed in
+            (videos) => {
+                //Use this data to set our state
+                //Syntactic sugar: videos is the same as videos: videos
+                this.setState({ videos });
+            }
+        );
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 //Append our component to the root node with a class container
